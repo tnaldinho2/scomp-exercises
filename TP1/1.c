df83d772ba015c399a2b802fe9962348e354472e
tomaz-x13 <tnaldinho@hotmail.com>
2024-05-06 16:33:23 +0100

    Exercise 1

--- TP1/1.c
+++ TP1/1.c
@@ -3,16 +3,29 @@
 #include <unistd.h>
 
 /*
-a) 4 processes 
-b) 
-c)
+a) 4 processes        P0        
+                    /    \  
+                  S0     S1 
+                  /  
+                GS0  
+b) For each process opened a "Computer Systems" string is printed to the terminal
 */
 
 
 int main(void){
     pid_t p, a;
     p = fork ();
+    if(p<0)
+    {
+      perror("'p' fork fail");
+      exit(1);
+    }
     a = fork ();
-    printf ("Computer Systems\n");
+    if(a<0)
+    {
+      perror("'a' fork fail");
+      exit(1);
+    }
+    printf ("Computer Systems (%d)\n", getpid());
     return 0;
 }
